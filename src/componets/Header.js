43dbba529c6c878e46784acf4fc67cd5ed45eb59
mplowey28 <matthew.plowey@gmail.com>
2020-10-14 19:54:47 +0200

    added styling

--- src/componets/Header.js
+++ src/componets/Header.js
@@ -5,9 +5,13 @@ const Header = () => {
     return (
        <header>
            <Nav />
-           <h1>Come take a ride</h1>
-           <p>Experience true engineering perfection</p>
-           <a href="#">Book Appointment</a>
+           <div className="content">
+            <div className="container">
+                <h1>Come take a ride</h1>
+                <p>Experience true engineering perfection</p>
+                <a href="#">Book Appointment</a>
+            </div>
+           </div>
        </header>
     )
 }
