8ee82f55882b0f88cd3c60c25610a83e12051c07
mplowey28 <matthew.plowey@gmail.com>
2020-10-15 13:30:18 +0200

    added styling

--- src/componets/Footer.js
+++ src/componets/Footer.js
@@ -2,20 +2,22 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <ul className="container">
-            <li>
-                <strong>Location</strong>
-                <p>Stockholm, Sweden</p>
-            </li>
-            <li>
-                <strong>Phone</strong>
-                <p>+46 000 000 0000</p>
-            </li>
-            <li>
-                <strong>Email</strong>
-                <p>cars@cars.cars</p>
-            </li>
-        </ul>
+        <footer>
+            <ul className="container">
+                <li>
+                    <strong>Location</strong>
+                    <p>Stockholm, Sweden</p>
+                </li>
+                <li>
+                    <strong>Phone</strong>
+                    <p>+46 000 000 0000</p>
+                </li>
+                <li>
+                    <strong>Email</strong>
+                    <p>cars@cars.cars</p>
+                </li>
+            </ul>
+        </footer>
     )
 }
 
