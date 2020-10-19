75ae37145f85790308d40fe990fe31fa7bb281b7
mplowey28 <64463512+mplowey28@users.noreply.github.com>
2020-10-19 11:06:41 +0200

    Update Nav.js

--- src/componets/Nav.js
+++ src/componets/Nav.js
@@ -1,32 +1,7 @@
 import React from 'react'
 import car from '../assests/icons/car.png'
-import styled from 'styled-components';
 
-const Ul = styled.ul`
-  list-style: none;
-  display: flex;
-  flex-flow: row nowrap;
-  li {
-    padding: 18px 10px;
-  }
-  @media (max-width: 768px) {
-    flex-flow: column nowrap;
-    background-color: #0D2538;
-    position: fixed;
-    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
-    top: 0;
-    right: 0;
-    height: 100vh;
-    width: 300px;
-    padding-top: 3.5rem;
-    transition: transform 0.3s ease-in-out;
-    li {
-      color: #fff;
-    }
-  }
-`;
-
-const Nav = ({ open }) => {
+const Nav = () => {
     return (
         <nav className="container">
             <div className="logo">
